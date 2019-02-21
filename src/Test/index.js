04a7f6cde1860a5e79f9956aecd78d0099fe4834
Alex Siharath <alex.siharath@yahoo.com>
2019-02-21 13:35:26 -0500

    ⚡️ It Works !!!

--- src/Test/index.js
+++ src/Test/index.js
@@ -1,18 +1,27 @@
 const fs = require('fs');
 const es = require('event-stream');
 
-let lines= [];
-
 class Test {
 
   countLines() {
-    fs.writeFileSync('./Data/sessionstest.txt', 'hey');
+    fs.writeFileSync('./Data/sessionstest.json', '[');
     fs.createReadStream('./Data/sessions.csv')
     .pipe(es.split())
     .pipe(
-      es.mapSync((line) => {
-        const string = `${line.split('\n')}\n`;
-        fs.appendFileSync('./Data/sessionstest.txt', string);
+      es.mapSync((file) => {
+        const lines = file.split('\n');
+        lines.forEach(element => {
+          const line = element.split(',');
+          const object = {
+            user_id: line[0],
+            action: line[1],
+            action_type: line[2],
+            action_detail: line[3],
+            device_type: line[4],
+            secs_elapsed: line[5]
+          };
+          fs.appendFileSync('./Data/sessionstest.json', `${JSON.stringify(object)},`);
+        });
       })
     )
     console.log('hey');
